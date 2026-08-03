224a9b323699130d53ac4e859e5ea6f67d30cf8e
MENORAH <mac@Menorahs-MacBook.local>
2026-08-03 16:16:19 +0100

    Add image optimization, Sole and Accessories filters, rename to Vintage Treasures

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,31 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
 
+  const Image = require("@11ty/eleventy-img").default || require("@11ty/eleventy-img");
+
+  async function imageShortcode(src, alt, sizes = "100vw") {
+    if (!src) return "";
+    let cleanSrc = src.replace(/^\//, "");
+    let metadata = await Image(cleanSrc, {
+      widths: [400, 800],
+      formats: ["jpeg"],
+      outputDir: "_site/images/optimized/",
+      urlPath: "/images/optimized/",
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    return Image.generateHTML(metadata, imageAttributes);
+  }
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+
   // Collections
   eleventyConfig.addCollection("products", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/products/*.md");
